26cbcc08b34fa2b8e646e3b212d1a7fb56585d54
Ichiro <ichirokozakai991106@gmail.com>
2022-11-15 17:00:06 +0900

    confirmed that Real, nominal and CPI normalized GDP

--- Jap_Python.py
+++ Jap_Python.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import gmean
+import matplotlib.pyplot as plt
 Jap_nominal_GDP = pd.read_csv(
     "C:/Users/ichir/Documents/seminar/final_paper_data/nominal_quater_gdp_Jap.csv")
 Year = np.arange(1994, 2023)
@@ -19,4 +21,21 @@ n = int(342/3)
 CPI_2015 = []
 for i in range(n):
     Q1 = [CPI_MONTH[3*i-2], CPI_MONTH[3*i-1], CPI_MONTH[3*i]]
-    Q2 =  # 幾何平均をとる
+    Q2 = gmean(Q1)
+    CPI_2015.append(Q2)
+Jap_nominal_GDP["CPI(2015based)"] = CPI_2015
+nominal_GDP = Jap_nominal_GDP["GDP"].tolist()
+Real_GDP = []
+for i in range(114):
+    Real_GDP.append(nominal_GDP[i] * 100 / CPI_2015[i])
+Jap_nominal_GDP["Real GDP"] = Real_GDP
+Jap_real_GDP = pd.read_csv(
+    "C:/Users/ichir/Documents/seminar/final_paper_data/Jap_Real_GDP.csv")
+Jap_real_GDP["FT"] = FT
+Jap_real_GDP = Jap_real_GDP.set_index("FT")
+print(Jap_real_GDP)
+plt.plot(Jap_nominal_GDP["GDP"], label="Nominal GDP")
+plt.plot(Jap_nominal_GDP["Real GDP"], label="CPI_Real GDP")
+plt.plot(Jap_real_GDP["GDP"], label="Real GDP")
+plt.legend()
+plt.show()
