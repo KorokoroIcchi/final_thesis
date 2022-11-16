7d5e7186f6c601aa9142caf90037d4244fc6f0ef
Ichiro <ichirokozakai991106@gmail.com>
2022-11-16 13:38:32 +0900

    delete the jl_file

--- Jap_GDP.jl
+++
@@ -1,7 +0,0 @@
-using CSV
-using DataFrames
-nominal_gdp = CSV.read("C:/Users/ichir/Documents/seminar/final_paper_data/Jap_Nominal_GDP.csv", DataFrame)
-eltype.(eachcol(nominal_gdp))
-rename!(nominal_gdp, [:FY, :GDP_expenditure, :Cp, :Ih, :Ip, :Iz_p, :Cg, :Ig, :Iz_g, :E, :M, :PDFP, :GDFP, :GFCF, :FINSAL])
-describe(nominal_gdp)
-names(nominal_gdp)[11:15]
